633399d5b68172cc3e040b1f14dcd4591d0d38d1
nathanaelatse <nathanael.atse.pro@outlook.com>
2026-03-18 10:45:41 +0000

    Test du SAST

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello sécurisé !");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Route XSS volontaire
+app.get("/hello", (req, res) => {
+  const name = req.query.name;
+  res.send("<h1>Bonjour " + name + "</h1>");
+});
+
+// Route open redirect volontaire
+app.get("/redirect", (req, res) => {
+  const url = req.query.url;
+  res.redirect(url);
+});
+
+// Route eval volontaire
+app.post("/calc", (req, res) => {
+  const expression = req.body.expression;
+  const result = eval(expression);
+  res.send("Résultat: " + result);
 });
 
 app.listen(3000, () => {
