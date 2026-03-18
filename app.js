819b3959a75cd1a4627036dcd2a1e8909693682b
nathanaelatse <nathanael.atse.pro@outlook.com>
2026-03-18 11:00:01 +0000

    Test du SAST et sortie codeql

--- app.js
+++ app.js
@@ -23,6 +23,43 @@ app.post("/calc", (req, res) => {
   res.send("Résultat: " + result);
 });
 
+ app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});  /*LA VERSION VULN DETECTABLE AVEC SAST*/
+
+/*const express = require("express");
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello sécurisé !");
+});
+
+app.get("/hello", (req, res) => {
+  const name = String(req.query.name || "");
+  res.type("text/plain").send(`Bonjour ${name}`);
+});
+
+app.get("/redirect", (req, res) => {
+  const url = req.query.url;
+
+  const allowedPaths = ["/", "/hello"];
+
+  if (!allowedPaths.includes(url)) {
+    return res.status(400).send("URL de redirection non autorisée");
+  }
+
+  res.redirect(url);
+});
+
+app.post("/calc", (req, res) => {
+  return res.status(400).json({
+    error: "Fonction désactivée : évaluation dynamique interdite"
+  });
+});
+
 app.listen(3000, () => {
   console.log("Server running on port 3000");
-});
+}); */
